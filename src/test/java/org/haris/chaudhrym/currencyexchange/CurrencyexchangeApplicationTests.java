0f4da46f51bbec2c798765b112c795a3a62503b1
harisux <haris.chaudhrym1@gmail.com>
2025-02-12 20:28:39 -0500

    feat: :tada: initializing spring project with required deps

---
+++ src/test/java/org/haris/chaudhrym/currencyexchange/CurrencyexchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.haris.chaudhrym.currencyexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyexchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
